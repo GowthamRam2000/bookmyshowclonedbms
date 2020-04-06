a176288168fc890ef6d20798895cac5ce8e9584f
Gowthamram <40654487+KabirNarayan@users.noreply.github.com>
2020-04-06 10:15:18 +0530

    Update Booking_Page2.java

--- src/Booking_Page2.java
+++ src/Booking_Page2.java
@@ -5,16 +5,6 @@ import java.sql.*;
 import java.util.Random;
 import javax.swing.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rohan
- */
 public class Booking_Page2 extends javax.swing.JFrame {
     
         String location="";
